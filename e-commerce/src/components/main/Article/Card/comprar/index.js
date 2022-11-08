d31d306f733d334a754f29725a81e1b0d0937444
catheali <aliciacatherinealencar@gmail.com>
2022-11-08 18:31:44 -0300

    linkto colecoes de destaque

--- e-commerce/src/components/main/Article/Card/comprar/index.js
+++ e-commerce/src/components/main/Article/Card/comprar/index.js
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
 import "./index.css";
 export default function Comprar(){
     return(
+        <Link to="/produtos">
         <button className="bttn">Comprar</button>
+        </Link>
     )
 }
